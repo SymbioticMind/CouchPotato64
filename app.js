f1538786b99bfd5213483f7ef91cc27dfa6191ab
SymbioticMind <amarler115@gmail.com>
2026-08-05 22:09:51 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+/*
+======================================
+ Couch Potato 64
+ Foundation Logic
+======================================
+*/
+
+
+console.log(
+"Couch Potato 64 Online"
+);
+
+
+
+function hostGame(){
+
+    alert(
+    "Host Game system coming soon!"
+    );
+
+}
+
+
+
+function joinGame(){
+
+    alert(
+    "Join Game system coming soon!"
+    );
+
+}
