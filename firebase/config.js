cd86919390f7c4bb8d42d3bb4f4a1431c843795f
SymbioticMind <amarler115@gmail.com>
2026-08-05 23:22:28 -0500

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,40 @@
+/*
+========================================
+ Couch Potato 64
+ Build 0.3.1
+
+ firebase/config.js
+ Firebase Connection Setup
+========================================
+*/
+
+
+// Replace everything inside the object
+// with your Firebase Web App config
+
+
+const firebaseConfig = {
+
+
+    apiKey: "YOUR_API_KEY",
+
+
+    authDomain: "YOUR_AUTH_DOMAIN",
+
+
+    databaseURL: "YOUR_DATABASE_URL",
+
+
+    projectId: "YOUR_PROJECT_ID",
+
+
+    storageBucket: "YOUR_STORAGE_BUCKET",
+
+
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+
+
+    appId: "YOUR_APP_ID"
+
+
+};
